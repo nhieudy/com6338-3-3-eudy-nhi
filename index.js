a033976d782a90f352abee0b55284a23553bfd27
Nhi Eudy <nhieudy@Mac.attlocal.net>
2025-09-10 16:52:20 -0500

    Edited alert, finalize result calculation and quiz function

--- index.js
+++ index.js
@@ -21,14 +21,15 @@ var questionsArr = [
     answer: false,
   },
 ];
-let correctAnswer = 0;
+
 function runQuiz() {
+let correctAnswer = 0; //Accidentally had this outside the function, so it didn't reset the score!
   for (let i = 0; i < questionsArr.length; i++) {
     let playerAnswer = confirm(questionsArr[i].question);
-    if (playerAnswer == questionsArr[i].answer){
-        correctAnswer += 1;
+    if (playerAnswer == questionsArr[i].answer) {
+      correctAnswer += 1;
     }
   }
-  let score = (correctAnswer/questionsArr.length)*100;
-  console.log(score);
+  let score = (correctAnswer / questionsArr.length) * 100;
+  alert("Your Results: " + Math.round(score) + "%");
 }
