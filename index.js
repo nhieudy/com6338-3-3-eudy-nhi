5a8399a4632bee2891fcc61f53e39f68b5e15b14
Nhi Eudy <nhieudy@Mac.attlocal.net>
2025-09-10 16:08:56 -0500

    added 5 questions array, created function

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+var questionsArr = [
+  {
+    question: "Are aquariums peaceful?",
+    answer: true
+  },
+  {
+    question: "Is the sky blue?",
+    answer: true
+  },
+  {
+    question: "Are blue jays native to the US?",
+    answer: true
+  },
+  {
+    question: "Was Thomas Jefferson our 2nd president?",
+    answer: false
+  },
+  {
+    question: "Can dogs eat grapes?",
+    answer: false
+  }
+]; //Array for questions, create objects - key-value pairs
+function runQuiz() {
+    for (let i = 0; i < questionsArr.length; i++){
+    let playerAnswer = confirm(question);
+    }
+}
